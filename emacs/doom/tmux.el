91a3eae46143e8cfc8f0ca7518d597dab988fc28
Mao <code@mao.com>
2020-04-20 23:00:36 +0800

    fix tmux pane config

--- emacs/doom/tmux.el
+++ emacs/doom/tmux.el
@@ -2,11 +2,11 @@
   :config
   (tmux-pane-mode)
   (map! :leader
-        (:prefix ("v" . "vpane")
+        (:prefix ("v" . "tmux pane")
           :desc "Open vpane" :nv "o" #'tmux-pane-open-vertical
           :desc "Open hpane" :nv "h" #'tmux-pane-open-horizontal
           :desc "Open hpane" :nv "s" #'tmux-pane-open-horizontal
-          :desc "Toggle vpane" :nv "v" #'tmux-pane-toggle-vertical
+          :desc "Open vpane" :nv "v" #'tmux-pane-open-vertical
           :desc "Close pane" :nv "c" #'tmux-pane-close
           :desc "Rerun last command" :nv "r" #'tmux-pane-rerun))
   (map! :leader
