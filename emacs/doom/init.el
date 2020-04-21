e6703c655dccdf14002faa8561d79f6eb04ac0dd
Mao <code@mao.com>
2020-04-21 14:17:28 +0800

    unicode module cause extra problems

--- emacs/doom/init.el
+++ emacs/doom/init.el
@@ -41,7 +41,7 @@
        pretty-code       ; replace bits of code with pretty symbols
        ;;tabs              ; an tab bar for Emacs
        treemacs          ; a project drawer, like neotree but cooler
-       unicode           ; extended unicode support for various languages
+       ;; unicode           ; extended unicode support for various languages
        vc-gutter         ; vcs diff in the fringe
        vi-tilde-fringe   ; fringe tildes to mark beyond EOB
        window-select     ; visually switch windows
