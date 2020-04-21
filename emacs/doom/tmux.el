20a35aff19fe53b308311a5a9e215a556d64a8eb
Mao <code@mao.com>
2020-04-21 15:34:25 +0800

    polish keymap description

--- emacs/doom/tmux.el
+++ emacs/doom/tmux.el
@@ -2,14 +2,14 @@
   :config
   (tmux-pane-mode)
   (map! :leader
-        (:prefix ("v" . "tmux pane")
-          :desc "Open vpane" :nv "o" #'tmux-pane-open-vertical
-          :desc "Open hpane" :nv "h" #'tmux-pane-open-horizontal
-          :desc "Open hpane" :nv "s" #'tmux-pane-open-horizontal
-          :desc "Open vpane" :nv "v" #'tmux-pane-open-vertical
+        (:prefix ("v" . "tmux")
+          :desc "Open vertical" :nv "o" #'tmux-pane-open-vertical
+          :desc "Open horizontal" :nv "h" #'tmux-pane-open-horizontal
+          :desc "Open vertical" :nv "v" #'tmux-pane-open-vertical
+          :desc "Open horizontal" :nv "s" #'tmux-pane-open-horizontal
           :desc "Close pane" :nv "c" #'tmux-pane-close
-          :desc "Rerun last command" :nv "r" #'tmux-pane-rerun))
+          :desc "Rerun command" :nv "r" #'tmux-pane-rerun))
   (map! :leader
         (:prefix "t"
-          :desc "vpane" :nv "v" #'tmux-pane-toggle-vertical
-          :desc "hpane" :nv "h" #'tmux-pane-toggle-horizontal)))
+          :desc "vertical tmux" :nv "v" #'tmux-pane-toggle-vertical
+          :desc "horizontal tmux" :nv "h" #'tmux-pane-toggle-horizontal)))
