4887e0fa20ff6d0f3dbe9c0d3c4eea5aab747f3e
Mao <code@mao.com>
2020-04-25 19:42:31 +0800

    sh compliance

--- zsh/profile.sh
+++ zsh/profile.sh
@@ -1,11 +1,12 @@
+#!/bin/sh
 # PATH will later be reordered in /etc/zprofile that calls /usr/libexec/path_helper
 LANG="en_US.UTF-8"
 LANGUAGE="en_US.UTF-8"
 LC_CTYPE="en_US.UTF-8"
 LC_ALL="en_US.UTF-8"
 
-if [[ -e ~/.brew.zsh ]]; then
-	source ~/.brew.zsh
+if [ -e "$HOME/.brew.zsh" ]; then
+	. "$HOME/.brew.zsh"
 	export PATH="$BREW_PREFIX/bin:$BREW_PREFIX/sbin":$PATH
 	export PATH="$BREW_PREFIX/opt/coreutils/libexec/gnubin":$PATH
 	export PATH="$BREW_PREFIX/opt/python/libexec/bin":$PATH
@@ -19,7 +20,7 @@ if [[ -e ~/.brew.zsh ]]; then
 	export FZF_BASE="$BREW_PREFIX/opt/fzf"
 fi
 
-if [[ -e ~/.emacs.d/bin ]]; then
+if [ -e "$HOME/.emacs.d/bin" ]; then
 	export PATH="$HOME/.emacs.d/bin":$PATH
 fi
 
@@ -28,7 +29,8 @@ export CARGO_HOME="$HOME/.cargo"
 if
 	hash rustup >/dev/null 2>&1
 then
-	export RUST_SRC_PATH="$(rustc --print sysroot)/lib/rustlib/src/rust/src"
+	RUST_SRC_PATH="$(rustc --print sysroot)/lib/rustlib/src/rust/src"
+	export RUST_SRC_PATH
 fi
 export RUST_BACKTRACE=1
 export PATH="$CARGO_HOME/bin":$PATH
@@ -41,18 +43,19 @@ export PATH="$HOME/bin":$PATH
 # 10ms for key sequences,
 # default 400ms cause latency when entering vi normal mode
 KEYTIMEOUT=1
+export KEYTIMEOUT
 
-test -e $HOME/.site/profile.zsh && source $HOME/.site/profile.zsh
+test -e "$HOME/.site/profile.zsh" && . "$HOME/.site/profile.zsh"
 
 # remove redundant path
 uniq_path() {
 	if [ -n "$PATH" ]; then
-		old_PATH=$PATH:
-		PATH=
+		old_PATH=$PATH
+		PATH=""
 		while [ -n "$old_PATH" ]; do
 			x=${old_PATH%%:*} # the first remaining entry
 			case $PATH: in
-			*:"$x":*) ;; # already there
+			*:"$x":*) ;;        # already there
 			*) PATH=$PATH:$x ;; # not there yet
 			esac
 			old_PATH=${old_PATH#*:}
