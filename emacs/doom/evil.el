e4c45a5b793880de3dc0c01408ce0d5275d5272c
Mao <code@mao.com>
2020-05-06 22:55:40 +0800

    clean up codes

--- emacs/doom/evil.el
+++ emacs/doom/evil.el
@@ -16,8 +16,6 @@
   (define-key evil-inner-text-objects-map
     evil-textobj-entire-key 'evil-entire-entire-buffer))
 
-(use-package! s)
-
 ;;;###autoload
 (defun replace-current-line ()
   "Replace the current line."
@@ -29,15 +27,16 @@
 
 (map! :nv "gl" #'replace-current-line)
 
-;; evil-collection for vterm overrided some keymaps defined by tmux-pane
 (add-hook! 'vterm-mode-hook
   (lambda()
+    ;; evil-collection for vterm overrided some keymaps defined by tmux-pane
     (evil-collection-define-key 'insert 'vterm-mode-map
       (kbd "C-c") 'vterm--self-insert
       (kbd "C-h") (lambda () (interactive) (tmux-pane--windmove "left" "tmux select-pane -L"))
       (kbd "C-j") (lambda () (interactive) (tmux-pane--windmove "down" "tmux select-pane -D"))
       (kbd "C-k") (lambda () (interactive) (tmux-pane--windmove "up" "tmux select-pane -U"))
       (kbd "C-l") (lambda () (interactive) (tmux-pane--windmove "right" "tmux select-pane -R")))
+    ;; change keymap to toggle sending escape to vterm
     (evil-collection-define-key '(normal insert) 'vterm-mode-map
       (kbd "C-q") 'evil-collection-vterm-toggle-send-escape)))
 
