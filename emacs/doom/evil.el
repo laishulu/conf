96e6c10986e304e5a889455d308709d9cab82632
Mao <code@mao.com>
2020-05-07 08:55:25 +0800

    fix add-hook! usage

--- emacs/doom/evil.el
+++ emacs/doom/evil.el
@@ -27,26 +27,26 @@
 
 (map! :nv "gl" #'replace-current-line)
 
-(add-hook! 'vterm-mode-hook
-  (lambda()
-    ;; evil-collection for vterm overrided some keymaps defined by tmux-pane
-    (evil-collection-define-key 'insert 'vterm-mode-map
-      (kbd "C-c") 'vterm--self-insert
-      (kbd "C-h") (lambda () (interactive) (tmux-pane--windmove
-                                       "left"
-                                       "tmux select-pane -L"))
-      (kbd "C-j") (lambda () (interactive) (tmux-pane--windmove
-                                       "down"
-                                       "tmux select-pane -D"))
-      (kbd "C-k") (lambda () (interactive) (tmux-pane--windmove
-                                       "up"
-                                       "tmux select-pane -U"))
-      (kbd "C-l") (lambda () (interactive) (tmux-pane--windmove
-                                       "right"
-                                       "tmux select-pane -R")))
-    ;; change keymap to toggle sending escape to vterm
-    (evil-collection-define-key '(normal insert) 'vterm-mode-map
-      (kbd "C-q") 'evil-collection-vterm-toggle-send-escape)))
+(add-hook!
+ 'vterm-mode-hook
+ ;; evil-collection for vterm overrided some keymaps defined by tmux-pane
+ (evil-collection-define-key 'insert 'vterm-mode-map
+   (kbd "C-c") 'vterm--self-insert
+   (kbd "C-h") (lambda () (interactive) (tmux-pane--windmove
+                                    "left"
+                                    "tmux select-pane -L"))
+   (kbd "C-j") (lambda () (interactive) (tmux-pane--windmove
+                                    "down"
+                                    "tmux select-pane -D"))
+   (kbd "C-k") (lambda () (interactive) (tmux-pane--windmove
+                                    "up"
+                                    "tmux select-pane -U"))
+   (kbd "C-l") (lambda () (interactive) (tmux-pane--windmove
+                                    "right"
+                                    "tmux select-pane -R")))
+ ;; change keymap to toggle sending escape to vterm
+ (evil-collection-define-key '(normal insert) 'vterm-mode-map
+   (kbd "C-q") 'evil-collection-vterm-toggle-send-escape))
 
 ;; override 'C-]' defined in evil-motion-state-map
 (define-key evil-motion-state-map "\C-]"
