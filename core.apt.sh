fdefd9aaaf33b3d3771def3ef4dbf0497892b8fc
Mao <code@mao.com>
2020-05-13 11:41:50 +0800

    add direnv

--- core.apt.sh
+++ core.apt.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-sudo apt install zsh fasd tmux neovim
-sudo apt install tree fzf ripgrep fd-find
+sudo apt install zsh fasd tmux neovim direnv
 sudo apt install cmake tig
+sudo apt install tree fzf ripgrep fd-find
--- core.sh
+++ core.sh
@@ -3,7 +3,7 @@
 if
 	type "brew" >/dev/null 2>&1
 then
-	brew install zsh fasd tmux neovim
+	brew install zsh fasd tmux neovim direnv
 	brew install cmake shfmt tig
 
 	# command line productivity
