dd2e3dcd081d6d19ea29566ff847091dc4b1687c
Mao <code@mao.com>
2020-05-27 18:51:47 +0800

    fix doom path

--- emacs/config.sh
+++ emacs/config.sh
@@ -3,8 +3,8 @@
 root_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" && cd .. && pwd)
 
 if [[ $overwrite == "yes" || ! -e ~/.emacs.d ]]; then
-    git clone -b develop https://github.com/hlissner/doom-emacs ~/.emacs.d
-    mkdir -p ~/.config
-    ln -sfn $root_dir/emacs/doom ~/.config/doom
-    doom install
+	git clone -b develop https://github.com/hlissner/doom-emacs ~/.emacs.d
+	mkdir -p ~/.config
+	ln -sfn $root_dir/emacs/doom ~/.config/doom
+	~/.emacs.d/bin/doom install
 fi
