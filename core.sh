fbe1b93178c6d3909c565e9a5468d4f8f466ab65
Mao <code@mao.com>
2020-05-30 21:26:14 +0800

    add shellcheck

--- core.sh
+++ core.sh
@@ -4,7 +4,7 @@ if
 	type "brew" >/dev/null 2>&1
 then
 	brew install zsh fasd tmux neovim direnv
-	brew install cmake shfmt tig
+	brew install cmake shfmt shellcheck tig
 
 	# command line productivity
 	brew install tree fzf bat ripgrep fd piknik
