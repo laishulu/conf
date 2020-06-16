44184184924803a0ce34b78fe64108cc23c5e5ec
Mao <code@mao.com>
2020-06-16 14:10:31 +0800

    clean codes

--- emacs/doom/edit.el
+++ emacs/doom/edit.el
@@ -11,8 +11,8 @@
   :config
   (smart-input-source-global-auto-english-mode t)
   (add-hook! '(text-mode-hook prog-mode-hook)
-             #'smart-input-source-inline-english-mode)
-  (add-hook! '(text-mode-hook prog-mode-hook)
              #'smart-input-source-remember-input-source-mode)
+  (add-hook! '(text-mode-hook prog-mode-hook)
+             #'smart-input-source-inline-english-mode)
   (add-hook! '(text-mode-hook prog-mode-hook)
              #'smart-input-source-follow-context-mode))
