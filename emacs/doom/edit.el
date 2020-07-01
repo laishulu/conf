1fc2804106e804357240fe097917770a456d62ee
Mao <code@mao.com>
2020-07-02 01:45:07 +0800

    update config to latest smart-input-source

--- emacs/doom/edit.el
+++ emacs/doom/edit.el
@@ -3,8 +3,17 @@
   (setq company-idle-delay 0.5))
 
 (use-package! smart-input-source
-  :after (evil)
+  :after (evil tmux-pane)
+  :init
   :config
+  (delete "C-h" smart-input-source-prefix-override-keys)
+  (setq smart-input-source-preserve-save-triggers
+        (append smart-input-source-preserve-save-triggers
+                (list 'tmux-pane-omni-window-last
+                      'tmux-pane-omni-window-up
+                      'tmux-pane-omni-window-down
+                      'tmux-pane-omni-window-left
+                      'tmux-pane-omni-window-right)))
   (smart-input-source-global-respect-mode t)
   (smart-input-source-global-inline-english-mode t)
   (smart-input-source-global-follow-context-mode t)
