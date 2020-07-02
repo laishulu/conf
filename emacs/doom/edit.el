51a6d568cfa878f8a541d46a8641f52d713c144b
Mao <code@mao.com>
2020-07-02 19:56:42 +0800

    clean config

--- emacs/doom/edit.el
+++ emacs/doom/edit.el
@@ -17,16 +17,4 @@
   (smart-input-source-global-respect-mode t)
   (smart-input-source-global-inline-english-mode t)
   (smart-input-source-global-follow-context-mode t)
-  (defvar original-cursor-background nil)
-  (add-hook 'smart-input-source-set-english-hook
-            (lambda ()
-              (when original-cursor-background
-                (set-cursor-color original-cursor-background))))
-  (add-hook 'smart-input-source-set-other-hook
-            (lambda ()
-              (unless original-cursor-background
-                (setq original-cursor-background
-                      (or (cdr (assq 'cursor-color default-frame-alist))
-                          (face-background 'cursor)
-                          "Red")))
-              (set-cursor-color "green"))))
+  (smart-input-source-global-cursor-color-mode t))
