7e4f6c3b87f41bf21bdf59fe5249c81e0a1ce43a
Mao <code@mao.com>
2020-07-13 01:49:31 +0800

    fix config

--- emacs/doom/edit.el
+++ emacs/doom/edit.el
@@ -7,14 +7,7 @@
   :init
   :config
   (delete "C-h" smart-input-source-prefix-override-keys)
-  (setq smart-input-source-preserve-save-triggers
-        (append smart-input-source-preserve-save-triggers
-                (list 'tmux-pane-omni-window-last
-                      'tmux-pane-omni-window-up
-                      'tmux-pane-omni-window-down
-                      'tmux-pane-omni-window-left
-                      'tmux-pane-omni-window-right)))
   (smart-input-source-global-respect-mode t)
-  (smart-input-source-global-inline-english-mode t)
+  (smart-input-source-global-inline-mode t)
   (smart-input-source-global-follow-context-mode t)
   (smart-input-source-global-cursor-color-mode t))
