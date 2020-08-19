ec3ab8b530fcb0551c8dc9ac7ae55b418982a4c7
Mao <code@mao.to>
2020-08-19 15:30:16 +0800

    install sk

--- core.sh
+++ core.sh
@@ -7,7 +7,7 @@ then
 	brew install cmake shfmt shellcheck tig
 
 	# command line productivity
-	brew install tree fzf skim bat ripgrep fd piknik
+	brew install tree fzf sk skim bat ripgrep fd piknik
 
 	# required by many tools
 	brew install socat
