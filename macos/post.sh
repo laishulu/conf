fa9280b6c85b910dbc59dce69a569b637355570f
Mao <code@mao.to>
2020-09-29 00:04:13 +0800

    fix fonts

--- macos/post.sh
+++ macos/post.sh
@@ -2,7 +2,10 @@
 
 # required to use clipboard in tmux
 brew install reattach-to-user-namespace
+
+# for fonts
 brew install fontconfig
-brew tap laishulu/fonts
-brew install sarasa-nerd
+brew tap laishulu/cask-fonts
+brew cask install font-sarasa-nerd
+
 brew cask install kitty
