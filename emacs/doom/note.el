53cac4c9ccc584c18c6b942d0a2b53bbeb96c3d2
Mao <code@mao.to>
2020-11-04 10:59:27 +0800

    lift keymap for notes

--- emacs/doom/note.el
+++ emacs/doom/note.el
@@ -16,3 +16,9 @@
            :file-name "${slug}"
            :head "#+title: ${title}\n\n"
            :unnarrowed t))))
+
+;; lift frequent keymap for notes
+(map! :leader
+      :desc "Open deft" "d" (general-simulate-key "SPC n d")
+      :desc "Org journal" "j" (general-simulate-key "SPC n j")
+      :desc "Org roam" "r" (general-simulate-key "SPC n r"))
