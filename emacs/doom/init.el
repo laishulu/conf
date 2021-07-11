800a1970584b7a3dfc69ceb652b5d37d0516aecd
Mao <code@mao.to>
2021-07-11 22:13:29 +0800

    update

--- emacs/doom/init.el
+++ emacs/doom/init.el
@@ -111,8 +111,8 @@
 
        :lang
        ;;agda              ; types of types of types of types...
-       ;;cc                ; C/C++/Obj-C madness
        ;;beancount         ; mind the GAAP
+       ;;cc                ; C/C++/Obj-C madness
        ;;clojure           ; java with a lisp
        ;;common-lisp       ; if you've seen one lisp, you've seen them all
        ;;coq               ; proofs-as-programs
