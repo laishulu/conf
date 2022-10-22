640efd4e7330ea5e055ad95eafb7ae38151cf539
Mao <code@mao.to>
2022-10-22 12:27:42 +0800

    suppress emacs org mode warning

--- emacs/doom/custom.el
+++ emacs/doom/custom.el
@@ -3,7 +3,18 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(package-lint)))
+ '(package-selected-packages '(package-lint))
+ '(warning-suppress-log-types
+   '((org-element-cache)
+     (org-element-cache)
+     (org-element-cache)
+     (org-element-cache)
+     (defvaralias)))
+ '(warning-suppress-types
+   '((org-element-cache)
+     (org-element-cache)
+     (org-element-cache)
+     (defvaralias))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
